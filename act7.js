b56c9f61b6f4bf7887c62e0b82ac2e85eea504a6
mateo-db <cabezasluciano752@gmail.com>
2026-05-28 01:38:39 -0300

    feat: se completó act 7

---
+++ act7.js
@@ -0,0 +1,14 @@
+//actividad 7
+const alumno = {
+    nombre: 'Agostina',
+    apellido: 'Gonzalez',
+    nota: 10
+}
+
+
+console.log(`
+    Apellido: ${alumno.apellido}
+    Nombre: ${alumno.nombre}
+    Nota: ${alumno.nota}
+    Estado: ${nota >= 6 ? 'Está aprobado!' : 'Está desaprobado'}
+`)
