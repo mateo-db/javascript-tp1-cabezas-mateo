aa96971cb1aff41ae02fcf660c903069dbc191f8
mateo-db <cabezasluciano752@gmail.com>
2026-05-28 01:37:26 -0300

    feat: se completó act 6

---
+++ act6.js
@@ -0,0 +1,8 @@
+//actividad 6
+for (let i = 1; i<=20; i++) {
+    if (i%2 === 0) {
+        console.log(`El numero ${i} es par!`)
+    } else {
+        console.log(`El numero ${i} es impar.`)
+    }
+}
