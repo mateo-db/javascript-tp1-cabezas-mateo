e58974c451505e230110bad597432d6724da723c
mateo-db <cabezasluciano752@gmail.com>
2026-05-28 01:30:21 -0300

    feat: se completó act 1

---
+++ act1.js
@@ -0,0 +1,11 @@
+//actividad 1
+const nombre = 'Cabezas Mateo';
+let edad, carrera, estoyCursando;
+edad = 23;
+carrera = 'Tecnicatura Superior en Desarrrollo de Software Multiplataforma'
+estoyCursando = 'si'
+
+
+
+
+console.log(`Soy ${nombre}, tengo ${edad} de edad. Estoy cursando la carrera de ${carrera}? La respuesta es ${estoyCursando}.`)
