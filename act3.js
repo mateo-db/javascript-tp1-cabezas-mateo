7059c134e9a3f7a33e7beff77cf2ddf1586b0121
mateo-db <cabezasluciano752@gmail.com>
2026-05-28 01:32:42 -0300

    feat: se completó act 3

---
+++ act3.js
@@ -0,0 +1,9 @@
+//actividad 3
+edad = 15;
+if (edad < 13) {
+    console.log('Niño');
+} else if (edad >= 13 && edad <= 17) {
+    console.log('Adolescente');
+} else {
+    console.log('Adulto');
+}
